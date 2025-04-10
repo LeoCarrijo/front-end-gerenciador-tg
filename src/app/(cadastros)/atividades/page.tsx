e4173b7570a97782755c8e2d25fcd418a06f95b7
Leonardo Carrijo <lh83712@gmail.com>
2025-04-10 06:44:30 -0300

    ✨ feat: React-hook-form nas Atividades
    Implementação do reaect-hook-form no form das atividades

--- src/app/(cadastros)/atividades/page.tsx
+++ src/app/(cadastros)/atividades/page.tsx
@@ -9,12 +9,24 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 import CardFatec from '@/components/CardFatec';
 import { Button } from '@/components/ui/button';
 import { CalendarIcon } from 'lucide-react';
 import { Calendar } from '@/components/ui/calendar';
 import { cn } from '@/lib/utils';
 import { format } from "date-fns"
+import { z } from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
 // Link para o schema do Prisma com os nomes das variáveis
 // https://github.com/MotahPedro/Gerenciador-de-TG/blob/develop/back-end/prisma/schema.prisma
@@ -22,52 +34,116 @@ import { format } from "date-fns"
 function Page() {
     const [date, setDate] = React.useState<Date>()
 
+    const formSchema = z.object({
+        matricula: z.coerce.number(),
+        dataEntrega: z.date(),
+        arquivo: z.any(),
+        descricao: z.string()
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            matricula: 0,
+            dataEntrega: new Date(),
+            arquivo: "",
+            descricao: ""
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
     return (
-        <CardFatec
-            description={`Cadastro e Envio de Atividades`}
-            buttonText={`Enviar`}
-            buttonFunction={() => { alert(`Enviado!`) }}
-        >
-            <div className="flex flex-col gap-1">
-                <Label htmlFor="matricula">RA</Label>
-                <Input name="matricula" id="matricula" type="number" placeholder="Digite o RA do aluno" />
-            </div>
-            <div className="flex flex-col gap-1">
-                <Label htmlFor="dataEntrega">Data de Entrega</Label>
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <Button
-                            name='dataEntrega'
-                            id='dataEntrega'
-                            variant={"outline"}
-                            className={cn(
-                                "w-full justify-start text-left font-normal",
-                                !date && "text-muted-foreground"
-                            )}
-                        >
-                            <CalendarIcon />
-                            {date ? format(date, "dd/MM/yyyy") : <span>dd/mm/aaaa</span>}
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                        <Calendar
-                            mode="single"
-                            selected={date}
-                            onSelect={setDate}
-                            className="rounded-md border p-2"
-                        />
-                    </PopoverContent>
-                </Popover>
-            </div>
-            <div className="flex flex-col gap-1">
-                <Label htmlFor="arquivo">Arquivo do Trabalho</Label>
-                <Input className='w-full' id="arquivo" name='arquivo' type="file" />
-            </div>
-            <div className="flex flex-col gap-1">
-                <Label htmlFor="descricao">Descrição da Atividade</Label>
-                <Textarea name="descricao" id="descricao" placeholder="Digite a descrição da atividade" />
-            </div>
-        </CardFatec>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <CardFatec
+                    description={`Cadastro de Aluno Orientando`}
+                    buttonText={`Cadastrar`}
+                    buttonFunction={() => { }}
+                >
+                    <FormField
+                        control={form.control}
+                        name="matricula"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormLabel>RA</FormLabel>
+                                    <FormControl>
+                                        <Input type="number" placeholder="Digite o RA do aluno" {...field} />
+                                    </FormControl>
+                                </FormItem>)
+                        }}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="dataEntrega"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormLabel>Data de Entrega</FormLabel>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <Button
+                                                name='dataEntrega'
+                                                id='dataEntrega'
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-full justify-start text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                <CalendarIcon />
+                                                {field.value ? format(field.value, "dd/MM/yyyy") : <span>dd/mm/aaaa</span>}
+                                            </Button>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={
+                                                    (value) => {
+                                                        field.onChange(value)
+                                                    }
+                                                }
+                                                className="rounded-md border p-2"
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </FormItem>
+                            )
+                        }}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="arquivo"
+                        render={({ field }) => {
+                            // TODO: Implementar método para subir o arquivo para o backup da Cloudflare (opcional)
+                            // TODO: Traduzir o texto do input file para pt-BR
+                            return (
+                                <FormItem>
+                                    <FormLabel htmlFor='arquivo'>Arquivo do Trabalho</FormLabel>
+                                    <Input className='w-full' type="file" {...field} />
+                                </FormItem>
+                            )
+                        }}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="descricao"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormLabel htmlFor='descricao'>Descrição da Atividade</FormLabel>
+                                    <Textarea placeholder="Digite a descrição da atividade" {...field} />
+                                </FormItem>
+                            )
+                        }}
+                    />
+                </CardFatec>
+            </form>
+        </Form>
     );
 }
 
