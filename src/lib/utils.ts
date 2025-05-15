b0e33eb4210629e04db6eafd8f09ba7bf49e0ab0
Leonardo Carrijo <lh83712@gmail.com>
2025-05-14 22:50:25 -0300

    ✨ feat: Importação do Sonner
    Importação do componente sonner/toast do shadcn/ui e a criação de uma função para padronizar as instanciacoes do toast.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function generateToast(message: string, success?: boolean) {
+  if (success == undefined) toast(message)
+  else if (success) toast.success(message, {style: {backgroundColor: "green", color: "white"}})
+  else toast.error(message, {style: {backgroundColor: "red", color: "white"}})
 }
