8dd516844e9e19aeec67ae5a867b7dbf615cca4c
Leonardo Carrijo <lh83712@gmail.com>
2025-05-16 12:14:04 -0300

    💄 style: Adicionando bordas
    Colocando bordas na tabela

--- src/app/(relatorios)/relatorios/orientador/page.tsx
+++ src/app/(relatorios)/relatorios/orientador/page.tsx
@@ -24,37 +24,39 @@ async function RelatorioOrientadorPage() {
 
     return (
         <CardFatecRelatorios description="Relatórios - Professores Orientadores">
-            <Table className='table-container'>
-                <TableHeader>
-                    <TableRow className='bg-muted/50'>
-                        <TableHead className='table-head-cell'>CPF do Orientador</TableHead>
-                        <TableHead className='table-head-cell'>Nome</TableHead>
-                        <TableHead className='table-head-cell'>E-mail</TableHead>
-                        <TableHead className='table-head-cell'>Linhas de Orientação</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {
-                        orientadores.map((orientador: Orientador) => (
-                            <TableRow key={orientador.cpf}>
-                                <TableCell className='table-body-cell'>{orientador.cpf}</TableCell>
-                                <TableCell className='table-body-cell'>{orientador.nome}</TableCell>
-                                <TableCell className='table-body-cell'>{orientador.email}</TableCell>
-                                <TableCell>
-                                    {
-                                        orientador.linhasOrientacao.map((linha: LinhaOrientacao) => (
-                                            <div key={linha.linha}>
-                                                <p className='py-1'>{linha.linha}</p>
-                                                <hr />
-                                            </div>
-                                        ))
-                                    }
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
+            <div className="table-container">
+                <Table>
+                    <TableHeader>
+                        <TableRow className='bg-muted/50'>
+                            <TableHead className='table-head-cell'>CPF do Orientador</TableHead>
+                            <TableHead className='table-head-cell'>Nome</TableHead>
+                            <TableHead className='table-head-cell'>E-mail</TableHead>
+                            <TableHead className='table-head-cell'>Linhas de Orientação</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {
+                            orientadores.map((orientador: Orientador) => (
+                                <TableRow key={orientador.cpf}>
+                                    <TableCell className='table-body-cell'>{orientador.cpf}</TableCell>
+                                    <TableCell className='table-body-cell'>{orientador.nome}</TableCell>
+                                    <TableCell className='table-body-cell'>{orientador.email}</TableCell>
+                                    <TableCell>
+                                        {
+                                            orientador.linhasOrientacao.map((linha: LinhaOrientacao) => (
+                                                <div key={linha.linha}>
+                                                    <p className='py-1'>{linha.linha}</p>
+                                                    <hr />
+                                                </div>
+                                            ))
+                                        }
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </div>
         </CardFatecRelatorios>
     )
 }
